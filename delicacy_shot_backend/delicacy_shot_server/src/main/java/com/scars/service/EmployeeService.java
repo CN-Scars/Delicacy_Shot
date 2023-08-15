50678f3ccf36a67ca6dfd35de1b1e06ee8ef9ba6
CN-Scars <yss2813483011xxl@gmail.com>
2023-08-15 16:22:08 +0800

    Realize the function of editing employee information, and the function of querying employee information for data echo on the front-end editing page

--- delicacy_shot_backend/delicacy_shot_server/src/main/java/com/scars/service/EmployeeService.java
+++ delicacy_shot_backend/delicacy_shot_server/src/main/java/com/scars/service/EmployeeService.java
@@ -35,4 +35,17 @@ public interface EmployeeService {
      * @return
      */
     void enableOrDisable(Integer status, long id);
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     */
+    void update(EmployeeDTO employeeDTO);
 }
