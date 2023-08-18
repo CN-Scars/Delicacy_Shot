aed33e19c1da7c5249749e4a8aafd3b7ed5528ae
CN-Scars <yss2813483011xxl@gmail.com>
2023-08-18 15:46:34 +0800

    Divide the interface documents of the management end and the user end to facilitate subsequent development

--- delicacy_shot_backend/delicacy_shot_server/src/main/java/com/scars/config/WebMvcConfiguration.java
+++ delicacy_shot_backend/delicacy_shot_server/src/main/java/com/scars/config/WebMvcConfiguration.java
@@ -49,16 +49,34 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      * @return
      */
     @Bean
-    public Docket docket() {
+    public Docket adminDocket() {
         ApiInfo apiInfo = new ApiInfoBuilder()
                 .title("吃得饱美味项目接口文档")
                 .version("1.0")
                 .description("吃得饱美味项目接口文档")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("管理端接口")
                 .apiInfo(apiInfo)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.scars.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.scars.controller.admin"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+
+    @Bean
+    public Docket userDocket() {
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("吃得饱美味项目接口文档")
+                .version("1.0")
+                .description("吃得饱美味项目接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("用户端接口")
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.scars.controller.user"))
                 .paths(PathSelectors.any())
                 .build();
         return docket;
