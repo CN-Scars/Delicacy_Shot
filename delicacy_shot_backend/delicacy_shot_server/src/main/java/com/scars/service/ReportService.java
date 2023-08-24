d70469735ce2ab75bd7d597260285e3ed1e042e5
CN-Scars <yss2813483011xxl@gmail.com>
2023-08-24 10:16:34 +0800

    Realize the function development of the turnover data statistics of the background management terminal

---
+++ delicacy_shot_backend/delicacy_shot_server/src/main/java/com/scars/service/ReportService.java
@@ -0,0 +1,15 @@
+package com.scars.service;
+
+import com.scars.vo.TurnoverReportVO;
+
+import java.time.LocalDate;
+
+public interface ReportService {
+    /**
+     * 营业额统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
+}
---
+++ delicacy_shot_backend/delicacy_shot_server/src/main/java/com/scars/service/impl/ReportServiceImpl.java
@@ -0,0 +1,58 @@
+package com.scars.service.impl;
+
+import com.scars.entity.Orders;
+import com.scars.mapper.OrderMapper;
+import com.scars.service.ReportService;
+import com.scars.vo.TurnoverReportVO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class ReportServiceImpl implements ReportService {
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 营业额统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
+        List<LocalDate> dateList = new ArrayList<>();
+        while (!begin.isAfter(end)) {
+            dateList.add(LocalDate.from(begin));
+            begin = begin.plusDays(1);
+        }
+
+        // 统计每天的营业额
+        List<Double> turnoverList = new ArrayList<>();
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+            Map map = new HashMap<>();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
+            map.put("status", Orders.COMPLETED);
+            Double turnover = orderMapper.sumByMap(map);
+            turnoverList.add(turnover == null ? 0.0 : turnover);
+        }
+
+        return TurnoverReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .turnoverList(StringUtils.join(turnoverList, ","))
+                .build();
+    }
+}
